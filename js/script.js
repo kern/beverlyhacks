35fe6fec78ac3470156d8548e13f5c4f3ffb3ca8
Shahan Khan <contact@shahan.me>
2015-03-04 18:51:50 -0800

    more cleanup and fixes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 // JavaScript Document
 $(document).ready(function() {
-  $('a[href^="#"]').on('click', function(e) {
-    e.preventDefault();
+    $('a[href^="#"]').on('click', function(e) {
+      e.preventDefault();
 
-    var target = this.hash;
-    $target = $(target);
+      var target = this.hash;
+      $target = $(target);
 
-    $('html, body').stop().animate({
-      'scrollTop': $target.offset().top
-    }, 900, 'swing', function() {
-      window.location.hash = target;
-    });
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+      }, 900, 'swing', function() {
+        window.location.hash = target;
+      });
 
-  $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="tooltip"]').tooltip();
+  });
 });
